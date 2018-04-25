d6fe28af75ce21d1c7007b4eee6ef9b9e451a782
clode <jackey8616@gmail.com>
2018-04-25 22:13:06 +0800

    Remove useless global variable

--- protocol/protocol.py
+++ protocol/protocol.py
@@ -1,41 +1,31 @@
 import sys, logging, json, struct, traceback
 
-names = '<PROTONAME>'
-messages = {}
-
 class Protocol:
 
     def __init__(self, name, peer):
-        self._name = self._nameRegister(name if name else '<PROTONAME>')
+        self._name = name
         self._peer = peer
         self._messages = {}
 
         self._messageExtand()
         self._messageRegister()
 
-    def _nameRegister(self, name):
-        global names
-        names = name
-        return names
-
     def _messageExtand(self):
         raise NotImplementedError
 
     def _messageRegister(self):
-        global messages
         for (name, message) in self._messages.items():
-            messages[name] = message
             setattr(self, name, message(self))
             self._messages[name] = getattr(self, name)
 
     def _wrap(self, *msg):
         (msgLen, msgType, msgData), = msg
-        return struct.pack('!12s4sL%ds' % msgLen, names.encode(), msgType.encode(), msgLen, msgData.encode())
+        return struct.pack('!12s4sL%ds' % msgLen, self._name.encode(), msgType.encode(), msgLen, msgData.encode())
 
     def wrapper(self, peerConn, msgType, pkType):
         try:
             msgLen, msgData = getattr(self, msgType).pack(pkType)
-            message = struct.pack('!12s4sL%ds' % msgLen, names.encode(), msgType.encode(), msgLen, msgData.encode())
+            message = struct.pack('!12s4sL%ds' % msgLen, self._name.encode(), msgType.encode(), msgLen, msgData.encode())
             return message
         except Exception as e:
             traceback.print_exc()
